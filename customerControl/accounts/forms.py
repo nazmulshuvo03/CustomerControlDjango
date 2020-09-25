88fe1374f8feda182d9dbdb40564cfd4460b004d
nazmulshuvo03 <nazmulshuvo03@gmail.com>
2020-09-25 16:30:41 +0600

    register and login complete

--- customerControl/accounts/forms.py
+++ customerControl/accounts/forms.py
@@ -1,8 +1,21 @@
 from django.forms import ModelForm
 from .models import Order
 
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
 
 class OrderForm(ModelForm):
     class Meta:
         model = Order
         fields = '__all__'
+
+
+class CreateUserForm(UserCreationForm):
+	"""User Registration Form"""
+		
+	class Meta:
+		model = User
+		fields = ['username', 'email', 'password1', 'password2']
+			
--- customerControl/accounts/views.py
+++ customerControl/accounts/views.py
@@ -2,11 +2,50 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
 
 # creating views
 from .models import *
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
+
+
+def registerPage(req):
+    form = CreateUserForm()
+
+    if(req.method == "POST"):
+        form = CreateUserForm(req.POST)
+        if form.is_valid():
+            form.save()
+
+            user = form.cleaned_data.get('username')
+            messages.success(req, 'Account created for ' + user)
+
+            return redirect('login')
+
+    context = {'form': form}
+
+    return render(req, 'accounts/register.html', context)
+
+
+def loginPage(req):
+
+    if (req.method == "POST"):
+        username = req.POST.get('username')
+        password = req.POST.get('password')
+
+        user = authenticate(req, username=username, password=password)
+
+        if user is not None:
+            login(req, user)
+            return redirect('home')
+
+    context = {}
+    return render(req, 'accounts/login.html', context)
 
 
 def home(req):
