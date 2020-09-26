1864487097943f26c0521ec8e634a0a9fbcc6fc1
nazmulshuvo03 <nazmulshuvo03@gmail.com>
2020-09-26 16:30:43 +0600

    signals added

--- customerControl/accounts/views.py
+++ customerControl/accounts/views.py
@@ -24,10 +24,6 @@ def registerPage(request):
             user = form.save()
             username = form.cleaned_data.get('username')
 
-            group = Group.objects.get(name="customer")
-            user.groups.add(group)
-            Customer.objects.create(user=user, name=user.username, email=user.email)
-
             messages.success(request, 'Account created for ' + username)
 
             return redirect('login')
